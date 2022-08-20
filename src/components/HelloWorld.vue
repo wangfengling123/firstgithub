27a08b967ec12667a5a7b76d404afddbe9352a57
Wangfengling <2968501652@qq.com>
2022-08-20 19:26:13 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="demo-input-suffix">
+    <el-row :gutter="20">
+      <span class="ml-3 w-35 text-gray-600 inline-flex items-center"
+        >Using attributes</span
+      >
+      <el-input
+        v-model="input1"
+        class="w-50 m-2"
+        placeholder="Pick a date"
+        :suffix-icon="Calendar"
+      />
+      <el-input
+        v-model="input2"
+        class="w-50 m-2"
+        placeholder="Type something"
+        :prefix-icon="Search"
+      />
+    </el-row>
+  </div>
+  <div class="demo-input-suffix">
+    <el-row :gutter="20">
+      <span class="ml-3 w-35 text-gray-600 inline-flex items-center"
+        >Using slots</span
+      >
+      <el-input v-model="input3" class="w-50 m-2" placeholder="Pick a date">
+        <template #suffix>
+          <el-icon class="el-input__icon"><calendar /></el-icon>
+        </template>
+      </el-input>
+      <el-input v-model="input4" class="w-50 m-2" placeholder="Type something">
+        <template #prefix>
+          <el-icon class="el-input__icon"><search /></el-icon>
+        </template>
+      </el-input>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Calendar, Search } from '@element-plus/icons-vue'
+const input1 = ref('')
+const input2 = ref('')
+const input3 = ref('')
+const input4 = ref('')
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="about">
+    <!-- 搜索按钮 -->
+    <div class="ax_default_shape">
+      <el-input
+    placeholder="请输入内容"
+    prefix-icon="el-icon-search"
+    v-model="input2">
+  </el-input>
+    </div>
+    <!-- 树形图 -->
+    <div class="ax_default_text">
+      <el-tree 
+        :data="data" 
+        :props="defaultProps" 
+        @node-click="handleNodeClick" 
+      />
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import { Search } from '@element-plus/icons-vue'
+const input2 = ref('')
+
+export default {
+  data() {
+    return {
+      input2,
+      Search,
+      data:[
+        {
+          label: 'Level one 1',
+          children: [
+            {
+              label: 'Level two 1-1',
+              children: [
+                {
+                  label: 'Level three 1-1-1',
+                  children:[
+                    {
+                      label:'Level four 1-1-1-1'
+                    }
+                  ]
+                },
+              ],
+            },
+          ],
+        }
+      ],
+      defaultProps:{
+        children: 'children',
+        label: 'label'
+      }
+    }
+  },
+  methods:{
+   handleNodeClick:(data) => {
+  console.log(data)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.ax_default_shape{
+    border-width: 0px;
+    position: absolute;
+    left: 289px;
+    top: 79px;
+    width: 240px;
+    height: 32px;
+    display: flex;
+    font-family: 'Microsoft YaHei Regular', 'Microsoft YaHei';
+    font-weight: 400;
+    font-style: normal;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.247058823529412);
+    text-align: left;
+    line-height: 22px;
+    .text{
+    position: absolute;
+    align-self: flex-start;
+    padding: 5px 0px 5px 12px;
+    box-sizing: border-box;
+    width: 100%;
+}
+}
+.ax_default_text{
+      border-width: 0px;
+    position: absolute;
+    left: 295px;
+    top: 120px;
+    width: 138px;
+    height: 38px;
+    display: flex;
+    font-size: 14px;
+    color: rgba(0, 0, 0, 0.647058823529412);
+    line-height: 22px;
+}
+</style>
